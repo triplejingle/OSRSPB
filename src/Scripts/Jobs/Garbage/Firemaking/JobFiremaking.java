6be8e5215ae71da58bf363f18ba7274ad846dac6
DESKTOP-8OHSR86\djima <dj.talahatu@student.han.nl>
2018-11-29 15:24:41 +0100

    init

---
+++ src/Scripts/Jobs/Garbage/Firemaking/JobFiremaking.java
@@ -0,0 +1,17 @@
+package Scripts.Jobs.Garbage.Firemaking;
+
+import Scripts.Core.JobCore;
+
+import org.powerbot.script.Script;
+
+
+public class JobFiremaking extends JobCore{
+        @Override
+        public void start() {
+
+        }
+}
+
+
+
+
